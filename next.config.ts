8e4bed90f2c52f40f4a85a4d18de29670e3f1d75
Njome-David <njomedavid26@gmail.com>
2026-04-23 00:39:31 +0100

    add: PWA features

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
+import withPWA from "@ducanh2912/next-pwa";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config) => config,
+  turbopack: {},
 };
 
-export default nextConfig;
+const pwaConfig = withPWA({
+  dest: "public",
+  register: true,
+  disable: process.env.NODE_ENV === "development",
+  workboxOptions: {
+    skipWaiting: true,        // ← Déplacé ici
+    clientsClaim: true,       // Optionnel : prend le contrôle immédiatement
+  },
+})(nextConfig);
+
+export default pwaConfig;
